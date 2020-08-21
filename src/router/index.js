49ad0a73676a732c391b19ef9fb7cb26a87ea352
GedeonV <zinni.arthur@gmail.com>
2020-08-21 11:54:55 +0200

    Ajout Route Panel

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Panel from '../views/Panel.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
+  {
+  	path: '/admin',
+  	name: 'Panel',
+  	component: Panel
+  }
 ]
+
 
 const router = new VueRouter({
   routes
