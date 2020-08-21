c64d27c19505d2b26707ab60931e6da20bd64ebe
GedeonV <zinni.arthur@gmail.com>
2020-08-21 11:32:31 +0200

    Import

--- src/main.js
+++ src/main.js
@@ -2,8 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './../node_modules/bulma/css/bulma.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$bus = new Vue()
+
+window.axios = axios.create({
+      baseURL: 'https://tools.sopress.net/iut/coop/api/',
+      params : {
+        token : false
+      },
+      headers: {
+        Authorization: ""
+      }
+    });
 
 new Vue({
   router,
