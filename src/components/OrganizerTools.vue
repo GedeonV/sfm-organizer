b505901a7e0fdfbd45a2b7766012d11200f1e3ca
GedeonV <zinni.arthur@gmail.com>
2020-08-21 12:04:50 +0200

    Ajout OrganizerTools

---
+++ src/components/OrganizerTools.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>TEST</div>
+</template>
+
+<script>
+	export default{
+		name: 'OrganizerTools',
+		methods :{
+
+		}, 
+		data(){
+			return {
+			
+			}
+		},
+		watch:{
+		    $route (to, from){
+		    
+		    }
+		}, 		
+		mounted(){
+			
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+	
+</style>
